9d6b875458b2d8e0071567a26dbfc89384ad023e
akaija <a.r.kaija@gmail.com>
2016-04-13 16:55:47 -0400

    Created branch, started with DB architecture

--- bin/generate.py
+++ bin/generate.py
@@ -5,7 +5,6 @@ import os
 import numpy as np
 from math import floor
 
-
 def GCD(a, b):
     while b:
         a, b = b, a % b
@@ -23,6 +22,7 @@ def LCMM(*args):
 def generate(
         N,
         ATOM_TYPES,
+        RUN_ID,
         ndenmax=0.084086,
         ndenmin=0.000013907,
         xmax=52.392,
@@ -37,9 +37,7 @@ def generate(
         sigmin=1.052,
         qmax=0.0,
         elem_charge=0.0001):
-    #
-    #    if type(N) != int:
-    #        print( 'N must be an
+
     Ntag = str(N)
     ntag = str(ndenmax)
     xtag = str(xmax)
@@ -49,44 +47,43 @@ def generate(
     sigtag = str(sigmax)
     qtag = str(qmax)
 
-#    top_path = (Ntag + 'mat_' + str(ATOM_TYPES) + 'atmtyp')
-    top_path = 'gen0'
+    wd = os.environ['HTSOHM_DIR']      # specify $HTSOHM_DIR as working directory
 
-    if not os.path.exists(top_path):
-        os.mkdir(top_path)
+    run_file = open(wd + '/' + RUN_ID + '.txt',"a")
+    run_file.write( "\nBOUNDARIES\n" +
+                    "Number density:\t%s\t-\t%s\n" % (ndenmin, ndenmax) +
+                    "Lattice constant, a:\t%s\t-\t%s\n" % (xmin, xmax) +
+                    "Lattice constant, b:\t%s\t-\t%s\n" % (ymin, ymax) +
+                    "Lattice constant, c:\t%s\t-\t%s\n" % (zmin, zmax) +
+                    "Epsilon:\t\t%s\t-\t%s\n" % (epmin, epmax) +
+                    "Sigma:\t\t\t%s\t-\t%s\n" % (sigmin, sigmax) +
+                    "Elemental charge:\t%s\n" % (elem_charge))
 
-    # open mat_stats.txt, to track material data
-    mat_stats = open(os.path.abspath(top_path) + '/mat_stats.txt', 'w')
-    mat_stat_heading = ('\nBOUNDARIES\nNumber of particles: ' + Ntag +
-                        '\nnumber density:   ' + ntag + '\nx-coordinate: ' +
-                        xtag + '\ny-coordinate: ' + ytag + '\nz-coordinate: ' +
-                        ztag + '\nEpsilon: ' + eptag + '\nSigma: ' + sigtag
-                        + '\nCharge: ' + qtag + '\n\n' +
-                        '#name     number density     xdim     ydim     ' +
-                        'zdim     total particles     net charge\n')
-    mat_stats.write(mat_stat_heading)
+    ff_dir = os.environ['FF_DIR']      # output force-field files to $FF_DIR
+    mat_dir = os.environ['MAT_DIR']    # output .cif-files to $MAT_DIR
 
-    # MAT-XXX loop...
-    for i in range(N):
-        mat_name = 'MAT-' + str(i)
+#    # open mat_stats.txt, to track material data
+#    mat_stats = open(os.path.abspath(top_path) + '/mat_stats.txt', 'w')
+#    mat_stat_heading = ('\nBOUNDARIES\nNumber of particles: ' + Ntag +
+#                        '\nnumber density:   ' + ntag + '\nx-coordinate: ' +
+#                        xtag + '\ny-coordinate: ' + ytag + '\nz-coordinate: ' +
+#                        ztag + '\nEpsilon: ' + eptag + '\nSigma: ' + sigtag
+#                        + '\nCharge: ' + qtag + '\n\n' +
+#                        '#name     number density     xdim     ydim     ' +
+#                        'zdim     total particles     net charge\n')
+#    mat_stats.write(mat_stat_heading)
 
-    # make MAT-XXX directory
-        os.mkdir(top_path + '/' + mat_name)
+    for i in range(N):                           # each iteration creates a new material
 
-    # open .cif file
-        cif_file = open(os.path.abspath(top_path) + '/' + mat_name + '/' +
-                        mat_name + '.cif', 'w')
+        mat_name = RUN_ID + '-' + str(i)
 
-    # open force_field_mixing_rules.def
-        mix_name = '/' + mat_name + '/force_field_mixing_rules.def'
-        mixing_rules = open(os.path.abspath(top_path) + mix_name, 'w')
+        def_dir = ff_dir + '/' + mat_name        # directory for material's force field
+        os.mkdir( def_dir )
 
-        psu_name = '/' + mat_name + '/pseudo_atoms.def'
-        pseudo_atoms = open(os.path.abspath(top_path) + psu_name, 'w')
-
-    # open force_field.def
-        ff_name = '/' + mat_name + '/force_field.def'
-        force_field = open(os.path.abspath(top_path) + ff_name, 'w')
+        cif_file = open(mat_dir + "/" + mat_name + ".cif", "w")        # structure file
+        mix_file = open(def_dir + '/force_field_mixing_rules.def', "w") # LJ-parameters
+        for_file = open(def_dir + '/force_field.def', "w")              # to overwrite LJ
+        psu_file = open(def_dir + '/psuedo_atoms.def', "w")             # define atom-types
 
         xdim_ = round(random() * (xmax - xmin) + xmin, 4)
         ydim_ = round(random() * (ymax - ymin) + ymin, 4)
@@ -96,50 +93,40 @@ def generate(
         n_ = randrange(2, Nmax, 1)
         nden_ = round(n_ / (xdim_ * ydim_ * zdim_))
 
-        cif_heading = (mat_name +
-                       '\n\nloop_\n' +
-                       '_symmetry_equiv_pos_as_xyz\n' +
-                       '  x,y,z\n' +
-                       '_cell_length_a          ' + str(xdim_) +
-                       '\n_cell_length_b          ' + str(ydim_) +
-                       '\n_cell_length_c          ' + str(zdim_) +
-                       '\n_cell_angle_alpha       90.0000\n' +
-                       '_cell_angle_beta        90.0000\n' +
-                       '_cell_angle_gamma       90.0000\n' +
-                       'loop_\n' +
-                       '_atom_site_label\n' +
-                       '_atom_site_type_symbol\n' +
-                       '_atom_site_fract_x\n' +
-                       '_atom_site_fract_y\n' +
-                       '_atom_site_fract_z\n')
-        cif_file.write(cif_heading)
+        cif_file.write( "\n\nloop_\n" +
+                        "_symmetry_equiv_pos_as_xyz\n" +
+                        "  x,y,z\n" +
+                        "_cell_length_a\t%s\n" % (xdim_) +
+                        "_cell_length_b\t%s\n" % (ydim_) +
+                        "_cell_length_c\t%s\n" % (zdim_) +
+                        "_cell_angle_alpha\t90.0000\n" +
+                        "_cell_angle_beta\t90.0000\n" +
+                        "_cell_angle_gamma\t90.0000\n" +
+                        "loop_\n" +
+                        "_atom_site_label\n" +
+                        "_atom_site_type_symbol\n" +
+                        "_atom_site_fract_x\n" +
+                        "_atom_site_fract_y\n" +
+                        "_atom_site_fract_z\n" )
 
-        mixing_heading = (
-            '# general rule for shifted vs truncated\n' +
-            'shifted\n' +
-            '# general rule tailcorrections\n' +
-            'no\n' +
-            '# number of defined interactions\n' +
-            str(
-                ATOM_TYPES +
-                8) +
-            '\n' +
-            '# type interaction, parameters.    IMPORTANT: define shortest matches first, so that more specific ones overwrites these\n')
-        mixing_rules.write(mixing_heading)
+        mix_file.write( "# general rule for shifted vs truncated\n" +
+                        "shifted\n" +
+                        "# general rule tailcorrections\n" +
+                        "no\n" +
+                        "# number of defined interactions\n" +
+                        "%s\n" % (ATOM_TYPES) +
+                        "# type interaction, parameters.    " + 
+                            "IMPORTANT: define shortest matches first, so" + 
+                            " that more specific ones overwrites these\n" )
 
-        pseudo_heading = ('#number of pseudo atoms\n' +
-                          str(ATOM_TYPES +
-                              8) +
-                          '\n#type          print    as     chem     oxidation' +
-                          '     mass       charge     polarization     ' +
-                          'B-factor     radii    connectivity     anisotropic' +
-                          '   anisotrop-type  tinker-type\n')
-        pseudo_atoms.write(pseudo_heading)
+        psu_file.write( "#number of pseudo atoms\n" +
+                        "%s\n" % (ATOM_TYPES + 8) +
+                        "#type\tprint\tas\tchem\toxidation\tmass\t" +
+                            "charge\tpolarization\tB-factor\tradii\t" +
+                            "connectivity\tanisotropic\tanisotrop-type\t" +
+                            "tinker-type\n" )
 
-# LJ parameters
-#####
-#####
-        ep = []
+        ep = []                        # assign LJ-parameters and partial charges
         sig = []
         q = []
 
@@ -151,17 +138,16 @@ def generate(
             charge = 0
             q.append(charge)
 
-        ep_ = np.asarray(ep)
-        sig_ = np.asarray(sig)
-        q_ = np.asarray(q)
-        ID_ = np.asarray(range(0, ATOM_TYPES))
+        ep_ = np.asarray(ep).reshape(-1, 1)
+        sig_ = np.asarray(sig).reshape(-1, 1)
+        q_ = np.asarray(q).reshape(-1, 1)
+        ID_ = np.asarray(range(0, ATOM_TYPES)).reshape(-1, 1)
+#        ep = ep_.reshape(-1, 1)
+#        sig = sig_.reshape(-1, 1)
+#        q = q_.reshape(-1, 1)
+#        ID = ID_.reshape(-1, 1)
 
-        ep = ep_.reshape(-1, 1)
-        sig = sig_.reshape(-1, 1)
-        q = q_.reshape(-1, 1)
-        ID = ID_.reshape(-1, 1)
-
-        atoms = np.hstack((ID, ep, sig, q))
+        atoms = np.hstack((ID_, ep_, sig_, q_))
 
         n_atoms = np.empty([0, 4])
 
@@ -216,10 +202,10 @@ def generate(
 
         mat_charge = str(round(net_q, 10))
 #        cif_file.write('#NET CHARGE: ' + mat_charge + '\n')
-        mat_X_stats = (mat_name + '     ' + str(nden_) + '     ' + str(xdim_) +
-                       '     ' + str(ydim_) + '     ' + str(zdim_) + '     ' +
-                       str(n_) + '     ' + mat_charge + '\n')
-        mat_stats.write(mat_X_stats)
+#        mat_X_stats = (mat_name + '     ' + str(nden_) + '     ' + str(xdim_) +
+#                       '     ' + str(ydim_) + '     ' + str(zdim_) + '     ' +
+#                       str(n_) + '     ' + mat_charge + '\n')
+#        mat_stats.write(mat_X_stats)
 
         eps = n_atoms[:, 1]
         sigs = n_atoms[:, 2]
@@ -227,19 +213,24 @@ def generate(
 
     # writing mixing_rules, pseudo_atoms...
         for i in range(ATOM_TYPES):
-            atom_X_pseudo = ('A_' +
-                             str(int(atoms[i, 0])) +
-                             '   yes   C   C   0   ' +
-                             '12.0   ' +
-                             str(atoms[i, 3]) +
-                             '   0.0   0.0   ' +
-                             '1.0  1.00   0   0  absolute   0\n')
-            pseudo_atoms.write(atom_X_pseudo)
+            psu_file.write( "A_%s\tyes\tC\tC\t0\t12.\t" % (int(atoms[i,0])) +
+                                "%s\t0\t0\t1\t1\t0\t0\t" % (atoms[i,3]) +
+                                "absolute\t0\n" )
+#            atom_X_pseudo = ('A_' +
+#                             str(int(atoms[i, 0])) +
+#                             '   yes   C   C   0   ' +
+#                             '12.0   ' +
+#                             str(atoms[i, 3]) +
+#                             '   0.0   0.0   ' +
+#                             '1.0  1.00   0   0  absolute   0\n')
+#            pseudo_atoms.write(atom_X_pseudo)
 
-            atom_X_mixing = ('A_' + str(int(atoms[i, 0])) + ' ' +
-                             'lennard-jones ' + str(atoms[i, 1]) + ' '
-                             + str(atoms[i, 2]) + '\n')
-            mixing_rules.write(atom_X_mixing)
+            mix_file.write( "A_%s\tlennard-jones\t" % (int(atoms[i,0])) +
+                                "%s\t%s\n" % (atoms[i,1], atoms[i,2]) )
+#            atom_X_mixing = ('A_' + str(int(atoms[i, 0])) + ' ' +
+#                             'lennard-jones ' + str(atoms[i, 1]) + ' '
+#                             + str(atoms[i, 2]) + '\n')
+#            mixing_rules.write(atom_X_mixing)
 
     # writing cif...
         for i in range(n_):
@@ -247,55 +238,60 @@ def generate(
             y = round(random(), 4)
             z = round(random(), 4)
 
-            atom_X_cif = ('A_' + str(int(n_atoms[i, 0])) + '     ' + 'C     ' +
-                          str(x) + '     ' + str(y) + '     ' + str(z) + '\n')
-            cif_file.write(atom_X_cif)
+            cif_file.write( "A_%s\tC\t" % (int(n_atoms[i,0])) +
+                                "%s\t%s\t%s\n" % (x, y, z) )
+#            atom_X_cif = ('A_' + str(int(n_atoms[i, 0])) + '     ' + 'C     ' +
+#                          str(x) + '     ' + str(y) + '     ' + str(z) + '\n')
+#            cif_file.write(atom_X_cif)
 
-    # SUPPORTED ADSORBATES
-    # name         pseudo-atoms
-    # N2       :   N_n2; N_com
-    # CO2      :   C_co2; O_co2
-    # methane  :   CH4_sp3
-    # helium   :   He
-    # hydrogen :   H_h2; H_com
-    # H2       :   H_h2; H_com
+# SUPPORTED ADSORBATES
+# name         pseudo-atoms
+# N2       :   N_n2; N_com
+# CO2      :   C_co2; O_co2
+# methane  :   CH4_sp3
+# helium   :   He
+# hydrogen :   H_h2; H_com
+# H2       :   H_h2; H_com
 
-        adsorbate_mixing = ('N_n2        lennard-jones   36.0     3.31\n' +
-                            'N_com       none\n' +
-                            'C_co2       lennard-jones   27.0     2.80\n' +
-                            'O_co2       lennard-jones   79.0     3.05\n' +
-                            'CH4_sp3     lennard-jones   158.5    3.72\n' +
-                            'He          lennard-jones   10.9     2.64\n' +
-                            'H_h2        none\n' +
-                            'H_com       lennard-jones   36.7     2.958\n' +
-                            '# general mixing rule for Lennard-Jones\n' +
-                            'Lorentz-Berthelot')
-        mixing_rules.write(adsorbate_mixing)
+        mix_file.write( "N_n2\tlennard-jones\t36.0\t3.31\n" +
+                        "N_com\tnone\n" +
+                        "C_co2\tlennard-jones\t27.0\t2.80\n" +
+                        "O_co2\tlennard-jones\t79.0\t3.05\n" +
+                        "CH4_sp3\tlennard-jones\t158.5\t3.72\n" +
+                        "He\tlennard-jones\t10.9\t2.64\n" +
+                        "H_h2\tnone\n" +
+                        "H_com\tlennard-jones\t36.7\t2.958\n" +
+                        "# general mixing rule for Lennard-Jones\n" +
+                        "Lorentz-Berthelot" )
+#        mixing_rules.write(adsorbate_mixing)
 
-        adsorbate_pseudo = ('N_n2     yes   N   N   0   14.00674   -0.4048' +
-                            '   0.0   1.0   0.7   0   0   relative   0\n' +
-                            'N_com    no    N   -   0   0.0         0.8096' +
-                            '   0.0   1.0   0.7   0   0   relative   0\n' +
-                            'C_co2    yes   C   C   0   12.0        0.70' +
-                            '     0.0   1.0   0.720 0   0   relative   0\n' +
-                            'O_co2    yes   O   O   0   15.9994    -0.35' +
-                            '     0.0   1.0   0.68  0   0   relative   0\n' +
-                            'CH4_sp3  yes   C   C   0   16.04246    0.0' +
-                            '      0.0   1.0   1.00  0   0   relative   0\n' +
-                            'He       yes   He  He  0   4.002602    0.0' +
-                            '      0.0   1.0   1.0   0   0   relative   0\n' +
-                            'H_h2     yes   H   H   0   1.00794     0.468' +
-                            '    0.0   1.0   0.7   0   0   relative   0\n' +
-                            'H_com    no    H   H   0   0.0        - 0.936' +
-                            '   0.0   1.0   0.7   0   0   relative   0\n')
-        pseudo_atoms.write(adsorbate_pseudo)
+        psu_file.write( "N_n2\tyes\tN\tN\t0\t14.00674\t-0.4048\t0.0\t1.0\t" +
+                            "0.7\t0\t0\trelative\t0\n" +
+                        "N_com\tno\tN\t-\t0\t0.0\t0.8096\t0.0\t1.0\t0.7\t" +
+                            "0\t0\trelative\t0\n" +
+                        "C_co2\tyes\tC\tC\t0\t12.0\t0.70\t0.0\t1.0\t0.720" +
+                            "\t0\t0\trelative\t0\n" +
+                        "O_co2\tyes\tO\tO\t0\t15.9994\t-0.35\t0.0\t1.0\t" +
+                            "0.68\t0\t0\trelative\t0\n" +
+                        "CH4_sp3\tyes\tC\tC\t0\t16.04246\t0.0\t0.0\t1.0\t" +
+                            "1.00\t0\t0\trelative\t0\n" +
+                        "He\tyes\tHe\tHe\t0\t4.002602\t0.0\t0.0\t1.0\t1.0\t" +
+                            "0\t0\trelative\t0\n" +
+                        "H_h2\tyes\tH\tH\t0\t1.00794\t0.468\t0.0\t1.0\t0.7" +
+                            "\t0\t0\trelative\t0\n" +
+                        "H_com\tno\tH\tH\t0\t0.0\t-0.936\t0.0\t1.0\t0.7\t0" +
+                            "\t0\trelative\t0\n" )
+#        pseudo_atoms.write(adsorbate_pseudo)
 
-        force_field_rules = ('# rules to overwrite\n0\n' +
-                             '# number of defined interactions\n0\n' +
-                             '# mixing rules to overwrite\n0')
-        force_field.write(force_field_rules)
+        for_file.write( "# rules to overwrite\n" +
+                        "0\n" +
+                        "# number of defined interactions\n" +
+                        "0\n" +
+                        "# mixing rules to overwrite\n" +
+                        "0" )
+#        force_field.write(force_field_rules)
 
         cif_file.close()
-        mixing_rules.close()
-        pseudo_atoms.close()
-        force_field.close()
+        mix_file.close()
+        psu_file.close()
+        for_file.close()
