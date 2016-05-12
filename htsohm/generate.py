897112a373aef0a8d1e8dd58b94c1b32d924ee36
akaija <a.r.kaija@gmail.com>
2016-05-12 15:28:27 -0400

    refactored htsohm/generate.py to have more readable logic

--- htsohm/generate.py
+++ htsohm/generate.py
@@ -1,5 +1,3 @@
-# /usr/bin/evn python
-
 import os
 
 from random import choice, random, randrange, randint
@@ -12,218 +10,195 @@ import yaml
 def gcd(a, b):
     while b:
         a, b = b, a % b
-
     return a
 
 def lcm(a, b):
-
     return a * b // gcd(a, b)
 
 def lcmm(*args):
-
     return reduce(lcm, args)
 
-def generate(number_of_materials, atom_types, run_id, 
-    max_number_density=0.084086, min_number_density=0.000013907, max_abc=52.392,
-    min_abc=13.098, max_epsilon=513.264, min_epsilon=1.258, max_sigma=6.549,
-    min_sigma=1.052, max_charge=0.0, elem_charge=0.0001):
-
+def write_material_config(run_id):
     wd = os.environ['HTSOHM_DIR']      # specify $HTSOHM_DIR as working directory
     config_file = os.path.join(wd, 'config', run_id + '.yaml')
-    config = {
-        "number-density-limits" : [min_number_density, max_number_density],
-        "lattice-constant-limits" : [min_abc, max_abc],
-        "epsilon-limits" : [min_epsilon, max_epsilon],
-        "sigma-limits" : [min_sigma, max_sigma],
-        "elemental-charge" : elem_charge}
+    material_config = {
+        "number-density-limits"     : [0.000013907, 0.084086],
+        "lattice-constant-limits"   : [13.098, 52.392],
+        "epsilon-limits"            : [1.258, 513.264],
+        "sigma-limits"              : [1.052, 6.549],
+        "charge-limit"              : 0.,
+        "elemental-charge"          : 0.0001}
     with open(config_file, "a") as file:
-        yaml.dump(config, file, default_flow_style=False)
+        yaml.dump(material_config, file, default_flow_style=False)
+    return material_config
 
+def random_value(limits):
+    x_min = limits[0]
+    x_max = limits[1]
+    x = round(random() * (x_max - x_min) + x_min, 4)
+    return x
+
+def random_number_density(number_density_limits, lattice_constants):
+    max_number_density = number_density_limits[1]
+    a = lattice_constants["a"]
+    b = lattice_constants["b"]
+    c = lattice_constants["c"]
+    max_number_of_atoms = int(max_number_density * a * b * c)
+    number_of_atoms = randrange(2, max_number_of_atoms, 1)
+    number_density = round(number_of_atoms / (a * b * c))
+    return number_of_atoms, number_density
+
+def write_cif_file(cif_file, lattice_constants, atom_sites):
+    with open(cif_file, "w") as file:
+        file.write( 
+            "\nloop_\n" +
+            "_symmetry_equiv_pos_as_xyz\n" +
+            "  x,y,z\n" +
+            "_cell_length_a\t%s\n" % lattice_constants["a"] +
+            "_cell_length_b\t%s\n" % lattice_constants["b"] +
+            "_cell_length_c\t%s\n" % lattice_constants["c"] +
+            "_cell_angle_alpha\t90.0000\n" +
+            "_cell_angle_beta\t90.0000\n" +
+            "_cell_angle_gamma\t90.0000\n" +
+            "loop_\n" +
+            "_atom_site_label\n" +
+            "_atom_site_type_symbol\n" +
+            "_atom_site_fract_x\n" +
+            "_atom_site_fract_y\n" +
+            "_atom_site_fract_z\n")
+        for atom_site in atom_sites:
+            chemical  = atom_site["chemical-id"]
+            x         = atom_site["x-frac"]
+            y         = atom_site["y-frac"]
+            z         = atom_site["z-frac"]
+            file.write("%s\t%s\t%s\t%s\n" % (chemical, x, y, z))
+
+def write_mixing_rules(mix_file, atom_types):
+    with open(mix_file, "w") as file:
+        file.write( 
+            "# general rule for shifted vs truncated\n" +
+            "shifted\n" +
+            "# general rule tailcorrections\n" +
+            "no\n" +
+            "# number of defined interactions\n" +
+            "%s\n" % (len(atom_types) + 8) +
+            "# type interaction, parameters.    " +
+            "IMPORTANT: define shortest matches first, so" +
+            " that more specific ones overwrites these\n")
+        for atom_type in atom_types:
+            atom_id    = atom_type["chemical-id"]
+            epsilon    = atom_type["epsilon"]
+            sigma      = atom_type["sigma"]
+            file.write(
+                "A_%s\tlennard-jones\t%s\t%s\n" % (atom_id, epsilon, sigma))
+        file.write(
+            "N_n2\tlennard-jones\t36.0\t3.31\n" +
+            "N_com\tnone\n" +
+            "C_co2\tlennard-jones\t27.0\t2.80\n" +
+            "O_co2\tlennard-jones\t79.0\t3.05\n" +
+            "CH4_sp3\tlennard-jones\t158.5\t3.72\n" +
+            "He\tlennard-jones\t10.9\t2.64\n" +
+            "H_h2\tnone\n" +
+            "H_com\tlennard-jones\t36.7\t2.958\n" +
+            "# general mixing rule for Lennard-Jones\n" +
+            "Lorentz-Berthelot")
+
+def write_pseudo_atoms(psu_file, atom_types):
+    with open(psu_file, "w") as file:
+        file.write(
+            "#number of pseudo atoms\n" +
+            "%s\n" % (len(atom_types) + 8) +
+            "#type\tprint\tas\tchem\toxidation\tmass\tcharge\tpolarization\tB-factor\tradii\t" +
+                 "connectivity\tanisotropic\tanisotrop-type\ttinker-type\n")
+        for atom_type in atom_types:
+            atom_id   = atom_type["chemical-id"]
+            charge    = atom_type["charge"]
+            file.write(
+                "A_%s\tyes\tC\tC\t0\t12.\t%s\t0\t0\t1\t1\t0\t0\tabsolute\t0\n" % (atom_id, charge))
+        file.write(
+            "N_n2\tyes\tN\tN\t0\t14.00674\t-0.4048\t0.0\t1.0\t0.7\t0\t0\trelative\t0\n" +
+            "N_com\tno\tN\t-\t0\t0.0\t0.8096\t0.0\t1.0\t0.7\t0\t0\trelative\t0\n" +
+            "C_co2\tyes\tC\tC\t0\t12.0\t0.70\t0.0\t1.0\t0.720\t0\t0\trelative\t0\n" +
+            "O_co2\tyes\tO\tO\t0\t15.9994\t-0.35\t0.0\t1.0\t0.68\t0\t0\trelative\t0\n" +
+            "CH4_sp3\tyes\tC\tC\t0\t16.04246\t0.0\t0.0\t1.0\t1.00\t0\t0\trelative\t0\n" +
+            "He\tyes\tHe\tHe\t0\t4.002602\t0.0\t0.0\t1.0\t1.0\t0\t0\trelative\t0\n" +
+            "H_h2\tyes\tH\tH\t0\t1.00794\t0.468\t0.0\t1.0\t0.7\t0\t0\trelative\t0\n" +
+            "H_com\tno\tH\tH\t0\t0.0\t-0.936\t0.0\t1.0\t0.7\t0\t0\trelative\t0\n")
+
+def write_force_field(for_file):
+    with open(for_file, "w") as file:
+        file.write(
+            "# rules to overwrite\n" +
+            "0\n" +
+            "# number of defined interactions\n" +
+            "0\n" +
+            "# mixing rules to overwrite\n" +
+            "0")
+
+def generate(run_id, number_of_materials, number_of_atomtypes):
+#    run_id                  = run_config["run-id"]
+#    number_of_materials     = run_config["children-per-generation"]
+#    number_of_atomtypes     = run_config["number-of-atom-types"]
+    material_config = write_material_config(run_id)
+    lattice_limits          = material_config["lattice-constant-limits"]
+    number_density_limits   = material_config["number-density-limits"]
+    epsilon_limits          = material_config["epsilon-limits"]
+    sigma_limits            = material_config["sigma-limits"]
+    max_charge              = material_config["charge-limit"]
+    elem_charge             = material_config["elemental-charge"]
+
+    wd = os.environ['HTSOHM_DIR']      # specify $HTSOHM_DIR as working directory
     ff_dir = os.environ['FF_DIR']      # output force-field files to $FF_DIR
     mat_dir = os.environ['MAT_DIR']    # output .cif-files to $MAT_DIR
-    for i in range(number_of_materials):         # each iteration creates a new material
-        mat_name = run_id + '-' + str(i)
-        def_dir = ff_dir + '/' + mat_name        # directory for material's force field
-        os.mkdir( def_dir )
-        cif_file = os.path.join(mat_dir, mat_name + ".cif")              # structure file
+
+    materials = []
+    for material in range(number_of_materials):           # each iteration creates a new material
+        material_id = run_id + '-' + str(material)
+
+        def_dir = os.path.join(ff_dir, material_id)       # directory for material's force field
+        os.mkdir(def_dir)
         mix_file = os.path.join(def_dir, 'force_field_mixing_rules.def') # LJ-parameters
         for_file = os.path.join(def_dir, 'force_field.def')              # to overwrite LJ
         psu_file = os.path.join(def_dir, 'pseudo_atoms.def')             # define atom-types
+        write_force_field(for_file)
 
-        xdim_ = round(random() * (max_abc - min_abc) + min_abc, 4)
-        ydim_ = round(random() * (max_abc - min_abc) + min_abc, 4)
-        zdim_ = round(random() * (max_abc - min_abc) + min_abc, 4)
+        cif_file = os.path.join(mat_dir, material_id + ".cif")              # structure file
+        lattice_constants = {"a" : random_value(lattice_limits),
+                             "b" : random_value(lattice_limits),
+                             "c" : random_value(lattice_limits)}
+        number_of_atoms, number_density = random_number_density(number_density_limits, 
+            lattice_constants)
 
-        n_max = int(max_number_density * xdim_ * ydim_ * zdim_)
-        n_ = randrange(2, n_max, 1)
-        nden_ = round(n_ / (xdim_ * ydim_ * zdim_))
+        atom_types = []
+        chemical_id = 0
+        for atom_type in range(number_of_atomtypes):
+            epsilon = random_value(epsilon_limits)
+            sigma = random_value(sigma_limits)
+            charge = 0.
+            atom_type = {
+                "chemical-id" : chemical_id,
+                "charge"      : charge,
+                "epsilon"     : epsilon,
+                "sigma"       : sigma}
+            atom_types.append(atom_type)
+            chemical_id += 1
+        write_mixing_rules(mix_file, atom_types)
+        write_pseudo_atoms(psu_file, atom_types)
 
-        with open(cif_file, "w") as file:
-            file.write( 
-                "\nloop_\n" +
-                "_symmetry_equiv_pos_as_xyz\n" +
-                "  x,y,z\n" +
-                "_cell_length_a\t%s\n" % (xdim_) +
-                "_cell_length_b\t%s\n" % (ydim_) +
-                "_cell_length_c\t%s\n" % (zdim_) +
-                "_cell_angle_alpha\t90.0000\n" +
-                "_cell_angle_beta\t90.0000\n" +
-                "_cell_angle_gamma\t90.0000\n" +
-                "loop_\n" +
-                "_atom_site_label\n" +
-                "_atom_site_type_symbol\n" +
-                "_atom_site_fract_x\n" +
-                "_atom_site_fract_y\n" +
-                "_atom_site_fract_z\n")
-
-        with open(mix_file, "w") as file:
-            file.write( 
-                "# general rule for shifted vs truncated\n" +
-                "shifted\n" +
-                "# general rule tailcorrections\n" +
-                "no\n" +
-                "# number of defined interactions\n" +
-                "%s\n" % (atom_types + 8) +
-                "# type interaction, parameters.    " +
-                "IMPORTANT: define shortest matches first, so" +
-                " that more specific ones overwrites these\n")
-
-        with open(psu_file, "w") as file:
-            file.write(
-                "#number of pseudo atoms\n" +
-                "%s\n" % (atom_types + 8) +
-                "#type\tprint\tas\tchem\toxidation\tmass\t" +
-                "charge\tpolarization\tB-factor\tradii\t" +
-                "connectivity\tanisotropic\tanisotrop-type\t" +
-                "tinker-type\n")
-
-        ep = []                        # assign LJ-parameters and partial charges
-        sig = []
-        q = []
-
-        for i in range(atom_types):
-            epsilon = round(random() * (max_epsilon - min_epsilon) + 
-                min_epsilon, 4)
-            ep.append(epsilon)
-            sigma = round(random() * (max_sigma - min_sigma) + min_sigma, 4)
-            sig.append(sigma)
-            charge = 0
-            q.append(charge)
-
-        ep_ = np.asarray(ep).reshape(-1, 1)
-        sig_ = np.asarray(sig).reshape(-1, 1)
-        q_ = np.asarray(q).reshape(-1, 1)
-        ID_ = np.asarray(range(0, atom_types)).reshape(-1, 1)
-
-        atoms = np.hstack((ID_, ep_, sig_, q_))
-
-        n_atoms = np.empty([0, 4])
-
-        for i in range(n_):
-            a_type = choice(range(atom_types))
-            n_atoms = np.vstack([n_atoms, atoms[a_type, :]])
-
-        a_count = []
-        a_id = []
-        for i in range(atom_types):
-            if i in n_atoms[:, 0]:
-                count = list(n_atoms[:, 0]).count(i)
-                if count != 0:
-                    a_count.append(count)
-                    a_id.append(i)
-
-        if len(a_count) != 1:            # more than one atom type!
-            temp_lcm = lcmm(*a_count)    # maxiumum charge multiplier
-            cm_max = floor(max_charge / (temp_lcm * elem_charge / min(a_count)))
-            cm_list = []
-            ac_len = len(a_count)
-
-            for i in range(ac_len - 1):  # choose 1st n-1 charge multipliers
-                cm_i = randint(-1 * cm_max, cm_max)
-                cm_list.append(cm_i)
-                atoms[a_id[i], 3] = cm_i * temp_lcm * elem_charge / a_count[i]
-
-            cm_f = -1 * sum(cm_list)
-            atoms[a_id[-1], 3] = cm_f * temp_lcm * elem_charge / a_count[-1]
-            net_q = 0
-            for i in range(ac_len):
-                net_q = a_count[i] * atoms[a_id[i], 3] + net_q
-
-        else:  # single atom type!
-            net_q = sum(atoms[:, 3])
-
-        mat_charge = str(round(net_q, 10))
-        eps = n_atoms[:, 1]
-        sigs = n_atoms[:, 2]
-
-        for i in range(atom_types):      # write mixing_rules and pseudo_atoms
-            with open(psu_file, "a") as file:
-                file.write(
-                    "A_%s\tyes\tC\tC\t0\t12.\t" % (int(atoms[i,0])) +
-                    "%s\t0\t0\t1\t1\t0\t0\t" % (atoms[i,3]) +
-                    "absolute\t0\n")
-
-            with open(mix_file, "a") as file:
-                file.write(
-                    "A_%s\tlennard-jones\t" % (int(atoms[i,0])) +
-                    "%s\t%s\n" % (atoms[i,1], atoms[i,2]))
-
-        for i in range(n_):              # write .cif-file
-            x = round(random(), 4)
-            y = round(random(), 4)
-            z = round(random(), 4)
-
-            with open(cif_file, "a") as file:
-                file.write(
-                    "A_%s\tC\t" % (int(n_atoms[i,0])) +
-                    "%s\t%s\t%s\n" % (x, y, z))
-
-# SUPPORTED ADSORBATES
-# name         pseudo-atoms
-# N2       :   N_n2; N_com
-# CO2      :   C_co2; O_co2
-# methane  :   CH4_sp3
-# helium   :   He
-# hydrogen :   H_h2; H_com
-# H2       :   H_h2; H_com
-
-        with open(mix_file, "a") as file:
-            file.write(
-                "N_n2\tlennard-jones\t36.0\t3.31\n" +
-                "N_com\tnone\n" +
-                "C_co2\tlennard-jones\t27.0\t2.80\n" +
-                "O_co2\tlennard-jones\t79.0\t3.05\n" +
-                "CH4_sp3\tlennard-jones\t158.5\t3.72\n" +
-                "He\tlennard-jones\t10.9\t2.64\n" +
-                "H_h2\tnone\n" +
-                "H_com\tlennard-jones\t36.7\t2.958\n" +
-                "# general mixing rule for Lennard-Jones\n" +
-                "Lorentz-Berthelot")
-
-        with open(psu_file, "a") as file:
-            file.write(
-                "N_n2\tyes\tN\tN\t0\t14.00674\t-0.4048\t0.0\t1.0\t" +
-                "0.7\t0\t0\trelative\t0\n" +
-                "N_com\tno\tN\t-\t0\t0.0\t0.8096\t0.0\t1.0\t0.7\t" +
-                "0\t0\trelative\t0\n" +
-                "C_co2\tyes\tC\tC\t0\t12.0\t0.70\t0.0\t1.0\t0.720" +
-                "\t0\t0\trelative\t0\n" +
-                "O_co2\tyes\tO\tO\t0\t15.9994\t-0.35\t0.0\t1.0\t" +
-                "0.68\t0\t0\trelative\t0\n" +
-                "CH4_sp3\tyes\tC\tC\t0\t16.04246\t0.0\t0.0\t1.0\t" +
-                "1.00\t0\t0\trelative\t0\n" +
-                "He\tyes\tHe\tHe\t0\t4.002602\t0.0\t0.0\t1.0\t1.0\t" +
-                "0\t0\trelative\t0\n" +
-                "H_h2\tyes\tH\tH\t0\t1.00794\t0.468\t0.0\t1.0\t0.7" +
-                "\t0\t0\trelative\t0\n" +
-                "H_com\tno\tH\tH\t0\t0.0\t-0.936\t0.0\t1.0\t0.7\t0" +
-                "\t0\trelative\t0\n")
-
-        with open(for_file, "w") as file:
-            file.write(
-                "# rules to overwrite\n" +
-                "0\n" +
-                "# number of defined interactions\n" +
-                "0\n" +
-                "# mixing rules to overwrite\n" +
-                "0")
+        atom_sites = []
+        for atom_site in range(number_of_atoms):
+            atom_type = choice(atom_types)
+            chemical_id = atom_type["chemical-id"]
+            x_fraction = round(random(), 4)
+            y_fraction = round(random(), 4)
+            z_fraction = round(random(), 4)
+            atom_site = {
+                "chemical-id" : chemical_id,
+                "x-frac"      : x_fraction,
+                "y-frac"      : y_fraction,
+                "z-frac"      : z_fraction}
+            atom_sites.append(atom_site)
+        write_cif_file(cif_file, lattice_constants, atom_sites)
+        material = {"atom-types" : atom_types, "atom-sites" : atom_sites}
+        materials.append(material)
+    return materials
