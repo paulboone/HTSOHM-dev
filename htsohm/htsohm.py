187c94387488096589684ac376f92992381dc07d
akaija <a.r.kaija@gmail.com>
2016-05-17 12:23:13 -0400

    fixed N-generation output, issue:

--- htsohm/htsohm.py
+++ htsohm/htsohm.py
@@ -40,26 +40,29 @@ def seed_generation(run_id, children_per_generation, number_of_atomtypes):
     session.commit()
 
 def next_generation(run_id, children_per_generation, generation):
+    s = session    # rename database objects to shorted queries
+    db = RunData
+
     for material in range(children_per_generation):
-        new_material = RunData(run_id, generation, 'none')
-        session.add(new_material)
-    session.commit()
+        new_material = db(run_id, generation, 'none')
+        s.add(new_material)
+    s.commit()
     status = "Dummy test:   RUNNING"
     while status == "Dummy test:   RUNNING":
         # Select parents, add IDs to database...
         next_generation_list = bng.select_parents(run_id, children_per_generation, generation)
-        session.commit()                                       # parent_ids staged by bng.select_parents()
+        s.commit()             # parent_ids staged by bng.select_parents()
         status = sim.dummy_test(run_id, next_generation_list, status, generation)
-        session.commit()                                       # within loop so "fail" parents aren't re-selected
+        s.commit()             # within loop so "fail" parents aren't re-selected
     if generation == 1:
-        mut.create_strength_array(run_id)                      # Create strength-parameter array `run_id`.npy
-    elif generation > 1:
-        mut.recalculate_strength_array(run_id, generation)     # Recalculate strength-parameters, as needed
-    mut.write_mutant_definition_files(run_id, generation)      # Create first generation of child-materials
-    children = session.query(RunData).filter(RunData.run_id == run_id, RunData.generation ==1).all()
+        mut.create_strength_array(run_id)                  # Create strength-parameter array `run_id`.npy
+    elif generation >= 2:
+        mut.recalculate_strength_array(run_id, generation) # Recalculate strength-parameters, as needed
+    mut.write_mutant_definition_files(run_id, generation)  # Create first generation of child-materials
+    children = s.query(db).filter(db.run_id == run_id, db.generation == generation).all()
     for material in children:
         sim.run_all_simulations(material.id)
-    session.commit()
+    s.commit()
 
 def htsohm(children_per_generation,    # number of materials per generation
            number_of_atomtypes,        # number of atom-types per material
