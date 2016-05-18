378de15758fd0599085b9b82a6061e900d1cb17c
Paul Boone <narcissus.pong@gmail.com>
2016-05-18 16:41:58 -0400

    add command run_hpc.py to run htsohm on hpc cluster using redis. run_hpc_py uses htsohm_hpc.py file to start and enqueue jobs (managed by python-rq). Mostly, this just imports the same methods used in the regular local serial htsohm.py script.

---
+++ htsohm/htsohm_hpc.py
@@ -0,0 +1,79 @@
+from time import sleep
+import yaml
+
+from redis import Redis
+from rq import Queue
+from rq.registry import StartedJobRegistry
+from sqlalchemy import func
+
+from htsohm.utilities import write_config_file, read_config_file
+from htsohm.htsohm import seed_generation, next_generation
+from htsohm.runDB_declarative import Material, session
+
+with open('hpc.yaml', 'r') as yaml_file:
+    hpc_config = yaml.load(yaml_file)
+
+redis_conn = Redis(**hpc_config['redis'])
+
+### NOTE: CAN CURRENTLY ONLY RUN ONE HTSOHM JOB ON A REDIS SERVER AT A TIME
+htsohm_queue = Queue('htsohm_queue', connection=redis_conn)
+
+
+def start_run(
+        children_per_generation,    # number of materials per generation
+        number_of_atomtypes,        # number of atom-types per material
+        strength_0,                 # intial strength parameter
+        number_of_bins,             # number of bins for analysis
+        max_generations=20):        # maximum number of generations
+
+    run_id = write_config_file(children_per_generation, number_of_atomtypes, strength_0,
+        number_of_bins, max_generations)['run-id']
+
+    print("starting run with run_id: %s" % run_id)
+    job = htsohm_queue.enqueue(manage_run,run_id, timeout=hpc_config['job_timeout'])
+
+def manage_run(run_id):
+    config = read_config_file(run_id)
+    last_generation = session.query(func.max(Material.generation)).filter(Material.run_id == run_id).one()[0]
+    if last_generation == None:
+        generation = 0
+    else:
+        generation = last_generation + 1
+
+    print("======================================================================================")
+    print("== manage_run, generation = %s" % generation)
+    print("======================================================================================")
+    #
+    # block until all jobs are complete
+    # the only started job in the queue should be THIS job!
+    registry = StartedJobRegistry(name='htsohm_queue', connection=redis_conn)
+    running_job_ids = registry.get_job_ids()
+    while len(running_job_ids) > 1:
+        print("still %s unfinished jobs in the htsohm queue; sleeping 60s..." % (len(running_job_ids) - 1))
+        sleep(60)
+        running_job_ids = registry.get_job_ids()
+
+    #
+    # prepare, mutate, and queue up the next generation
+    if generation > config['max-number-of-generations']:
+        print("max generations exceeded; we're done here!")
+        return
+    elif generation == 0:
+        # SEED GENERATION
+        seed_generation(run_id,
+                        config['children-per-generation'], config['number-of-atom-types'],
+                        queue=htsohm_queue)
+    elif generation >= 1:
+        # FIRST GENERATION, AND ON...
+        next_generation(run_id,
+                        config['children-per-generation'], generation,
+                        queue=htsohm_queue)
+
+    #
+    # seed_generation or next_generation will have enqueued all the materials jobs, but now we
+    # enqueue another manage_run job to run after they are all completed.
+    htsohm_queue.enqueue(manage_run, run_id)
+
+    print("======================================================================================")
+    print("== END manage_run, generation = %s" % generation)
+    print("======================================================================================")
---
+++ run_hpc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+
+from htsohm.htsohm_hpc import start_run
+
+if __name__ == "__main__":
+    start_run(int(sys.argv[1]),
+              int(sys.argv[2]),
+              float(sys.argv[3]),
+              int(sys.argv[4]))
