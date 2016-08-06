b4d0d186c0e87a5d35ebb7d5f64c4c3aad4738e9
Paul Boone <narcissus.pong@gmail.com>
2016-08-06 15:47:26 -0400

    number of cycles for production run

--- htsohm/methane_loading_simulation.py
+++ htsohm/methane_loading_simulation.py
@@ -6,8 +6,8 @@ def write_raspa_file(filename, run_id, material_id, helium_void_fraction ):
     with open(filename, "w") as config:
         config.write(
             "SimulationType\t\t\tMonteCarlo\n" +
-            "NumberOfCycles\t\t\t100\n" +             # number of MonteCarlo cycles
-            "NumberOfInitializationCycles\t50\n" +    # number of initialization cycles
+            "NumberOfCycles\t\t\t1000\n" +             # number of MonteCarlo cycles
+            "NumberOfInitializationCycles\t1000\n" +    # number of initialization cycles
             "PrintEvery\t\t\t10\n" +
             "RestartFile\t\t\tno\n" +
             "\n" +
