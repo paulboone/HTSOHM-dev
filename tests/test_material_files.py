e87d7d8654838c46bdae8a6d54e2e96fcb3b283b
akaija <a.r.kaija@gmail.com>
2016-12-05 15:27:10 -0500

    latest version of test

--- tests/test_material_files.py
+++ tests/test_material_files.py
@@ -5,32 +5,15 @@ from htsohm.material_files import random_number_density
 
 @pytest.fixture
 def LCs():
-    #==============================================================================
-    # default limits (see htsohm.sample.yaml)
-    LC_limits = [25.6, 51.2]
-
-    #==============================================================================
-    # pick random lattice-constans within limits
-    LCs = {
-        'a'           : uniform(*LC_limits),
-        'b'           : uniform(*LC_limits),
-        'c'           : uniform(*LC_limits)
+    lattice_constants = {
+        'a' : 1,
+        'b' : 1,
+        'c' : 1
     }
-    LCs['volume'] = LCs['a'] * LCs['b'] * LCs['c']
-    return LCs
+    return lattice_constants
 
-@pytest.fixture
-def ND_limits():
-    #==========================================================================
-    # default limits (see htsohm.sample.yaml)
-    limits = [1.49e-05, 0.02122]
-    return limits
+def test_number_density_bounds():
+    assert random_number_density((100000, 100000), LCs) == 100000
 
-def test_boundaries(LCs, ND_limits):
-#    for i in range(0, 10):
-     number_density = random_number_density(ND_limits, LCs) / LCs['volume']
-     assert number_density >= ND_limits[0] and number_density <= ND_limits[1]
-
-def test_two_site_constraint(LCs, ND_limits):
-#    for i in range(0, 10):
-    assert random_number_density(ND_limits, LCs) >= 2
+def test_two_site_constraint():
+    assert random_number_density((0, 1), LCs) == 2 and random_number_density((0, 2), LCs) == 2
