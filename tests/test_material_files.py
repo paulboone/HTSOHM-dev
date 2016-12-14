71b5907df32fe21dacefd11d010baa18ae89800b
A Reino Kaija <a.r.kaija@gmail.com>
2016-12-14 14:47:30 -0500

    fixed number density test

--- tests/test_material_files.py
+++ tests/test_material_files.py
@@ -5,15 +5,12 @@ from htsohm.material_files import random_number_density
 
 @pytest.fixture
 def LCs():
-    lattice_constants = {
-        'a' : 1,
-        'b' : 1,
-        'c' : 1
-    }
+    lattice_constants = {"a" : 1, "b" : 1, "c" : 1}
     return lattice_constants
 
 def test_number_density_bounds():
-    assert random_number_density((100000, 100000), LCs) == 100000
+    assert random_number_density((100000, 100000), LCs()) == 100000
 
 def test_two_site_constraint():
-    assert random_number_density((0, 1), LCs) == 2 and random_number_density((0, 2), LCs) == 2
+    assert random_number_density((1, 2), LCs()) == 2
+
