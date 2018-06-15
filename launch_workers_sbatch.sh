8bc8c89d40ad250105cf5e7460f282a4da86528e
akaija <a.r.kaija@gmail.com>
2018-06-15 17:15:52 -0400

    added shell script for launching jobs on slurm

---
+++ launch_workers_sbatch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# $Revision: 1.0 $
+# $Date:  2017-09-11 $
+# $Author: akaija $
+
+#SBATCH -N 1
+#SBATCH -t 1-00:00 # Runtime in D-HH:MM
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=2g
+
+echo JOB_ID: $SLURM_JOB_ID JOB_NAME: $SLURM_JOB_NAME HOSTNAME: $SLURM_SUBMIT_HOST
+echo start_time: `date`
+
+# dependencies
+module purge
+module load python/3.5.1
+module load postgresql/9.5.2
+source ~/venv/htsohm/bin/activate
+
+cd $SLURM_SUBMIT_DIR
+for ((i = 0; i < $SLURM_CPUS_ON_NODE; i++))
+do
+    echo TASK NO.: $i
+    ./hts.py launch_worker ${SLURM_JOB_NAME} >> ${SLURM_JOB_NAME}/output_${SLURM_SUBMIT_HOST}_$$_$i.log 2>&1 &
+done
+
+wait
