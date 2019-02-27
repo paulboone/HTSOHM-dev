84cd94db743369e65150fac7569be23359a2158a
Paul Boone <narcissus.pong@gmail.com>
2019-02-27 13:11:56 -0500

    changed colors: white lattice, black density, blue atom types, orange atom sites

--- htsohm/figures.py
+++ htsohm/figures.py
@@ -63,7 +63,7 @@ def delaunay_figure(box_r, convergence_bins, output_path, triang=None, children=
         ax.add_patch(Circle((0.5, 0.5), 0.125, fill=False, linestyle="dashed", linewidth=1, zorder=50))
         ax.add_patch(Circle((0.5, 0.5), 0.375, fill=False, linestyle="dashed", linewidth=2, zorder=50))
 
-    p_cmap = {"lattice": "#F9F7F3", "atom_types": "#EDDEA4", "atom_sites": "#F7A072", "density": "#0FA3B1"}
+    p_cmap = {"lattice": "w", "density": "black", "atom_types": "#0FA3B1", "atom_sites": "#F77936", }
     arrows = [ConnectionPatch(parents[i], children[i], "data", arrowstyle="-|>", \
                 shrinkA=5, shrinkB=5, mutation_scale=20, fc=p_cmap[perturbation_methods[i]], \
                 linestyle="--", zorder=40) \
