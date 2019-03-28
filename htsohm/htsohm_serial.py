377164ffdeb1f6d074a9c22c1e957b4c2f6c96c3
Paul Boone <narcissus.pong@gmail.com>
2019-03-28 18:01:34 -0400

    implemented restart

--- htsohm/htsohm_serial.py
+++ htsohm/htsohm_serial.py
@@ -41,6 +41,13 @@ def calc_bins(box_r, num_bins, prop1range=(0.0, 1.0), prop2range=(0.0, 1.0)):
 def empty_lists_2d(x,y):
     return [[[] for j in range(x)] for i in range(y)]
 
+def dump_restart(path, box_d, box_r, bin_counts, bin_materials, bins, gen):
+    np.savez(path, box_d, box_r, bin_counts, bin_materials, bins, gen)
+
+def load_restart(path):
+    npzfile = np.load(path)
+    return [npzfile[v] if npzfile[v].size != 1 else npzfile[v].item() for v in npzfile.files]
+
 def serial_runloop(config_path):
     """
     Args:
@@ -54,62 +61,66 @@ def serial_runloop(config_path):
     db.init_database(config["database_connection_string"])
     session = db.get_session()
 
-    num_bins = config['number_of_convergence_bins']
-    bin_counts = np.zeros((num_bins, num_bins))
-    bin_materials = empty_lists_2d(num_bins, num_bins)
-
+    print(config)
     children_per_generation = config['children_per_generation']
     # prop1 = config['prop1']
     # prop2 = config['prop2']
     prop1range = config['prop1range']
     prop2range = config['prop2range']
+    verbose = config['verbose'] if 'verbose' in config else False
+    num_bins = config['number_of_convergence_bins']
+    benchmarks = config['benchmarks']
+    next_benchmark = benchmarks.pop(0)
+    last_benchmark_reached = False
+    load_restart_path = config['load_restart_path'] if 'load_restart_path' in config else False
 
     print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()))
 
-    verbose = config['verbose'] if 'verbose' in config else False
-    benchmarks = config['benchmarks']
-    next_benchmark = benchmarks.pop(0)
+    if load_restart_path:
+        print("Restarting from file: %s" % load_restart_path)
+        box_d, box_r, bin_counts, bin_materials, bins, start_gen = load_restart(load_restart_path)
+        print("Restarting at generation %d" % start_gen)
+        print("There are currently %d materials" % len(box_r))
+    else:
 
-    print(config)
+        # define variables that are needed for state
+        bin_counts = np.zeros((num_bins, num_bins))
+        bin_materials = empty_lists_2d(num_bins, num_bins)
+        box_d = np.zeros(children_per_generation, dtype=int)
+        box_r = -1 * np.ones((children_per_generation, 2))
+        bins = set()
 
-    # generate initial generation of random materials
-    box_d = np.zeros(children_per_generation, dtype=int)
-    box_r = -1 * np.ones((children_per_generation, 2))
+        # generate initial generation of random materials
+        if config['initial_points_random_seed']:
+            print("applying random seed to initial points: %d" % config['initial_points_random_seed'])
+            random.seed(config['initial_points_random_seed'])
 
-    if config['initial_points_random_seed']:
-        print("applying random seed to initial points: %d" % config['initial_points_random_seed'])
-        random.seed(config['initial_points_random_seed'])
+        for i in range(children_per_generation):
+            print("Material Index: ", i)
+            material = pseudomaterial_generator.random.new_material(run_id, config["structure_parameters"])
+            run_all_simulations(material, config)
+            session.add(material)
+            session.commit()
 
-    for i in range(children_per_generation):
-        print("Material Index: ", i)
-        material = pseudomaterial_generator.random.new_material(run_id, config["structure_parameters"])
-        run_all_simulations(material, config)
-        session.add(material)
-        session.commit()
+            box_d[i] = material.id
+            box_r[i,:] = (material.void_fraction[0].void_fraction, material.gas_loading[0].absolute_volumetric_loading)
+            # box_r[i,:] = (material[prop1], material[prop2])
 
-        box_d[i] = material.id
-        box_r[i,:] = (material.void_fraction[0].void_fraction, material.gas_loading[0].absolute_volumetric_loading)
-        # box_r[i,:] = (material[prop1], material[prop2])
+        random.seed() # flush the seed so that only the initial points are set, not generated points
 
-    random.seed() # flush the seed so that only the initial points are set, not generated points
+        all_bins = calc_bins(box_r, num_bins, prop1range=prop1range, prop2range=prop2range)
+        for i, (bx, by) in enumerate(all_bins):
+            bin_counts[bx,by] += 1
+            bin_materials[bx][by].append(i)
+        bins = set(all_bins)
 
-    all_bins = calc_bins(box_r, num_bins, prop1range=prop1range, prop2range=prop2range)
-    for i, (bx, by) in enumerate(all_bins):
-        bin_counts[bx,by] += 1
-        bin_materials[bx][by].append(i)
-    bins = set(all_bins)
-    print("bins", bins)
+        output_path = os.path.join(config['output_dir'], "triplot_0.png")
+        delaunay_figure(box_r, num_bins, output_path, bins=bin_counts, \
+                        title="Starting random materials", \
+                        prop1range=prop1range, prop2range=prop2range)
+        start_gen = 1
 
-    output_path = os.path.join(config['output_dir'], "triplot_0.png")
-    delaunay_figure(box_r, num_bins, output_path, bins=bin_counts, \
-                    title="Starting random materials", \
-                    prop1range=prop1range, prop2range=prop2range)
-
-
-    last_benchmark_reached = False
-
-    for gen in range(1, config['max_generations'] + 1):
-
+    for gen in range(start_gen, config['max_generations'] + 1):
         parents_r = parents_d = []
         perturbation_methods = None
         if config['selector_type'] == 'simplices-or-hull':
@@ -117,7 +128,6 @@ def serial_runloop(config_path):
             perturbation_methods = [""] * children_per_generation
         elif config['selector_type'] == 'density-bin':
             parents_d, parents_r = selector_bin.choose_parents(children_per_generation, box_d, box_r, bin_materials)
-            print("parents_d: ", parents_d)
             perturbation_methods = [""] * children_per_generation
 
         # mutate materials and simulate properties
@@ -128,7 +138,6 @@ def serial_runloop(config_path):
             if config['generator_type'] == 'random':
                 material = pseudomaterial_generator.random.new_material(run_id, config["structure_parameters"])
             elif config['generator_type'] == 'mutate':
-                print("parents_d[i]: ", parents_d[i])
                 material = pseudomaterial_generator.mutate.mutate_material(run_id, parents_d[i], config["structure_parameters"])
                 perturbation_methods[i] = material.perturbation
 
@@ -153,7 +162,7 @@ def serial_runloop(config_path):
         bin_fraction_explored = len(bins) / num_bins ** 2
         if verbose:
             print_block('%s GENERATION %s: %5.2f%%' % (run_id, gen, bin_fraction_explored * 100))
-        if bin_fraction_explored >= next_benchmark:
+        while bin_fraction_explored >= next_benchmark:
             print_block("%s: %5.2f%% exploration accomplished at generation %d" %
                 ('{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()), bin_fraction_explored * 100, gen))
             if benchmarks:
@@ -184,6 +193,9 @@ def serial_runloop(config_path):
 
         box_d = np.append(box_d, new_box_d, axis=0)
         box_r = np.append(box_r, new_box_r, axis=0)
+
+        restart_path = os.path.join(config['output_dir'], "restart_%d.txt.npz" % gen)
+        dump_restart(restart_path, box_d, box_r, bin_counts, bin_materials, bins, gen + 1)
 
         if last_benchmark_reached:
             break
