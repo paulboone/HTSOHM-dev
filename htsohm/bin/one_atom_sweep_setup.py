bd2a46b4663eb8e6a7e8ebdec13b832f5200b809
Paul Boone <narcissus.pong@gmail.com>
2019-06-14 09:44:51 -0400

    allow sweep points to vary by dimension

--- htsohm/bin/one_atom_sweep_setup.py
+++ htsohm/bin/one_atom_sweep_setup.py
@@ -14,10 +14,17 @@ def sweep_materials(config_path):
     db.init_database(config["database_connection_string"])
     session = db.get_session()
 
+    if 'sweep_points' in config:
+        lattice_sweep_points = sigma_sweep_points = epsilon_sweep_points = config['sweep_points']
+    else:
+        lattice_sweep_points = config['lattice_sweep_points']
+        sigma_sweep_points = config['sigma_sweep_points']
+        epsilon_sweep_points = config['epsilon_sweep_points']
+
     scfg = config['structure_parameters']
-    eps_d = np.linspace(*scfg['epsilon_limits'], config['sweep_points'])
-    sig_d = np.linspace(*scfg['sigma_limits'], config['sweep_points'])
-    a_d = np.linspace(*scfg['lattice_constant_limits'], config['sweep_points'])
+    eps_d = np.linspace(*scfg['epsilon_limits'], epsilon_sweep_points)
+    sig_d = np.linspace(*scfg['sigma_limits'], sigma_sweep_points)
+    a_d = np.linspace(*scfg['lattice_constant_limits'], lattice_sweep_points)
     # always do symmetrical with one-atom only
     lattice_coords = [(a, a, a) for a in a_d]
 
