556eb6a10381d11e386c3c4082a3b6f57315265f
Paul Boone <narcissus.pong@gmail.com>
2019-06-14 11:02:25 -0400

    restart until certain error threshold is reached

--- htsohm/simulation/gas_loading.py
+++ htsohm/simulation/gas_loading.py
@@ -8,12 +8,14 @@ from string import Template
 import sys
 from uuid import uuid4
 
+import numpy as np
+
 from htsohm.material_files import write_mol_file, write_mixing_rules
 from htsohm.material_files import write_pseudo_atoms, write_force_field
 from htsohm.simulation.files import load_and_subs_template
 from htsohm.db import GasLoading
 
-def write_raspa_file(filename, material, simulation_config):
+def write_raspa_file(filename, material, simulation_config, restart):
     """Writes RASPA input file for simulating gas adsorption.
 
     Args:
@@ -32,9 +34,10 @@ def write_raspa_file(filename, material, simulation_config):
     # Load simulation parameters from config
     unit_cells = material.structure.minimum_unit_cells(simulation_config['cutoff'])
     values = {
+            "Restart"                       : 'yes' if restart else 'no',
             "Cutoff"                        : simulation_config['cutoff'],
             "NumberOfCycles"                : simulation_config["simulation_cycles"],
-            "NumberOfInitializationCycles"  : simulation_config["initialization_cycles"],
+            "NumberOfInitializationCycles"  : simulation_config["initialization_cycles"] if not restart else 0,
             "FrameworkName"                 : material.uuid,
             "HeliumVoidFraction"            : void_fraction,
             "ExternalTemperature"           : simulation_config["temperature"],
@@ -49,11 +52,12 @@ def write_raspa_file(filename, material, simulation_config):
     with open(filename, "w") as raspa_input_file:
         raspa_input_file.write(input_data)
 
-def write_output_files(material, simulation_config, output_dir):
+def write_output_files(material, simulation_config, output_dir, restart, filename=None):
     # Write simulation input-files
     # RASPA input-file
-    filename = os.path.join(output_dir, "{}_loading.input".format(simulation_config['adsorbate']))
-    write_raspa_file(filename, material, simulation_config)
+    if filename is None:
+        filename = os.path.join(output_dir, "{}_loading.input".format(simulation_config['adsorbate']))
+    write_raspa_file(filename, material, simulation_config, restart)
     # Pseudomaterial mol-file
     write_mol_file(material, output_dir)
     # Lennard-Jones parameters, force_field_mixing_rules.def
@@ -80,10 +84,11 @@ def parse_output(output_file, material, simulation_config):
     gas_loading.adsorbate        = simulation_config["adsorbate"]
     gas_loading.pressure         = simulation_config["pressure"]
     gas_loading.temperature      = simulation_config["temperature"]
+    atom_blocks = []
 
     with open(output_file) as origin:
-        line_counter = 1
-        for line in origin:
+        lines = origin.read().split('\n')
+        for i, line in enumerate(lines):
             if "absolute [mol/kg" in line:
                 gas_loading.absolute_molar_loading = float(line.split()[5])
             elif "absolute [cm^3 (STP)/g" in line:
@@ -98,36 +103,33 @@ def parse_output(output_file, material, simulation_config):
             elif "excess [cm^3 (STP)/c" in line:
                 gas_loading.excess_volumetric_loading  = float(line.split()[6])
             elif "Average Host-Host energy:" in line:
-                host_host_line = line_counter + 8
+                line = lines[i + 8]
+                gas_loading.host_host_avg = float(line.split()[1])
+                gas_loading.host_host_vdw = float(line.split()[5])
+                gas_loading.host_host_cou = float(line.split()[7])
             elif "Average Adsorbate-Adsorbate energy:" in line:
-                adsorbate_adsorbate_line = line_counter + 8
+                line = lines[i + 8]
+                gas_loading.adsorbate_adsorbate_avg = float(line.split()[1])
+                gas_loading.adsorbate_adsorbate_vdw = float(line.split()[5])
+                gas_loading.adsorbate_adsorbate_cou = float(line.split()[7])
             elif "Average Host-Adsorbate energy:" in line:
-                host_adsorbate_line = line_counter + 8
-            line_counter += 1
+                line = lines[i + 8]
+                gas_loading.host_adsorbate_avg  = float(line.split()[1])
+                gas_loading.host_adsorbate_vdw  = float(line.split()[5])
+                gas_loading.host_adsorbate_cou  = float(line.split()[7])
+            elif "Number of molecules:" in line:
+                atom_blocks = [float(lines[offset + i + 5].split()[2]) for offset in range(5)]
+            elif "Conversion factor molecules/unit cell -> cm^3 STP/cm^3:" in line:
+                atoms_uc_to_vv = float(line.split()[7])
 
         print("\n{} LOADING : {} v/v (STP)".format(simulation_config["adsorbate"],
                                             gas_loading.absolute_volumetric_loading))
         if material.parent:
             print("(parent LOADING : {} v/v (STP))".format(material.parent.gas_loading[0].absolute_volumetric_loading))
         print("\n")
-    with open(output_file) as origin:
-        line_counter = 1
-        for line in origin:
-            if line_counter == host_host_line:
-                gas_loading.host_host_avg = float(line.split()[1])
-                gas_loading.host_host_vdw = float(line.split()[5])
-                gas_loading.host_host_cou = float(line.split()[7])
-            elif line_counter == adsorbate_adsorbate_line:
-                gas_loading.adsorbate_adsorbate_avg = float(line.split()[1])
-                gas_loading.adsorbate_adsorbate_vdw = float(line.split()[5])
-                gas_loading.adsorbate_adsorbate_cou = float(line.split()[7])
-            elif line_counter == host_adsorbate_line:
-                gas_loading.host_adsorbate_avg  = float(line.split()[1])
-                gas_loading.host_adsorbate_vdw  = float(line.split()[5])
-                gas_loading.host_adsorbate_cou  = float(line.split()[7])
-            line_counter += 1
 
-    material.gas_loading.append(gas_loading)
+
+    return gas_loading, atom_blocks, atoms_uc_to_vv
 
 def pressure_string(p):
     if p >= 10 ** 6:
@@ -148,9 +150,12 @@ def run(material, simulation_config, config):
     adsorbate = simulation_config["adsorbate"]
     output_dir = "output_{}_{}".format(material.uuid, uuid4())
     os.makedirs(output_dir, exist_ok=True)
+    raspa_config = "./{}_loading.input".format(adsorbate)
+    raspa_restart_config = "./{}_loading_restart.input".format(adsorbate)
 
-    # RASPA input-file
-    write_output_files(material, simulation_config, output_dir)
+    # RASPA input-files
+    write_output_files(material, simulation_config, output_dir, restart=False, filename=os.path.join(output_dir, raspa_config))
+    write_output_files(material, simulation_config, output_dir, restart=True, filename=os.path.join(output_dir, raspa_restart_config))
 
     # Run simulations
     print("Date             : {}".format(datetime.now().date().isoformat()))
@@ -160,15 +165,53 @@ def run(material, simulation_config, config):
     print("Pressure         : {}".format(simulation_config["pressure"]))
     print("Temperature      : {}".format(simulation_config["temperature"]))
 
-    subprocess.run(["simulate", "-i", "./{}_loading.input".format(adsorbate)], check=True, cwd=output_dir)
+    unit_cells = material.structure.minimum_unit_cells(simulation_config['cutoff'])
+    total_unit_cells = unit_cells[0] * unit_cells[1] * unit_cells[2]
+    all_atom_blocks = []
 
-    data_files = glob(os.path.join(output_dir, "Output", "System_0", "*.data"))
-    if len(data_files) != 1:
-        raise Exception("ERROR: There should only be one data file in the output directory for %s. Check code!" % output_dir)
-    output_file = data_files[0]
+    subprocess.run(["simulate", "-i", raspa_config], check=True, cwd=output_dir)
+    for i in range(simulation_config['max_restarts'] + 1):
 
-    # Parse output
-    parse_output(output_file, material, simulation_config)
+        data_files = glob(os.path.join(output_dir, "Output", "System_0", "*.data"))
+        if len(data_files) != 1:
+            raise Exception("ERROR: There should only be one data file in the output directory for %s. Check code!" % output_dir)
+        output_file = data_files[0]
+
+        # Parse output
+        gas_loading, atom_blocks, atoms_uc_to_vv = parse_output(output_file, material, simulation_config)
+        print("atoms_uc_to_vv = %f" % atoms_uc_to_vv)
+
+        all_atom_blocks += atom_blocks
+        print("All blocks: ", all_atom_blocks)
+
+        atoms_std = np.std(np.mean(np.array(all_atom_blocks).reshape(-1, i+1), axis=1))
+        print("std of all blocks avg %d: %f" % (i, np.std(all_atom_blocks)))
+        print("std of all blocks: %f" % np.std(all_atom_blocks))
+
+        error_vv = atoms_std * atoms_uc_to_vv / total_unit_cells
+        gas_loading.absolute_volumetric_loading_error = error_vv
+        print("calculated error: %f" % error_vv)
+
+        # remove old RestartInitial directory and copy the current one to there
+        shutil.rmtree(os.path.join(output_dir, "RestartInitial"), ignore_errors=True)
+        shutil.copytree(os.path.join(output_dir, "Restart"), os.path.join(output_dir, "RestartInitial"))
+
+        # backup RASPA outputs to restart index
+        shutil.move(os.path.join(output_dir, "Output"), os.path.join(output_dir, "Output-%d" % i))
+        shutil.move(os.path.join(output_dir, "Restart"), os.path.join(output_dir, "Restart-%d" % i))
+        shutil.move(os.path.join(output_dir, "Movies"), os.path.join(output_dir, "Movies-%d" % i))
+        shutil.move(os.path.join(output_dir, "VTK"), os.path.join(output_dir, "VTK-%d" % i))
+
+        if gas_loading.absolute_volumetric_loading_error < simulation_config['restart_err_threshold']:
+            # gas_loading.cycles = simulation_config['simulation_cycles'] * (i + 1)
+            break
+
+        subprocess.run(["simulate", "-i", raspa_restart_config], check=True, cwd=output_dir)
+
+
+
+    material.gas_loading.append(gas_loading)
+
     if not config['keep_configs']:
         shutil.rmtree(output_dir, ignore_errors=True)
     sys.stdout.flush()
