52f2546309a956bd4944b5d9e03d8ed82c77f74d
Paul Boone <narcissus.pong@gmail.com>
2019-07-02 11:53:50 -0400

    fix to iterator so atoms beyond the first work

--- tests/test_void_fraction.py
+++ tests/test_void_fraction.py
@@ -13,4 +13,12 @@ def test_void_fraction_huge_atom():
 
 def test_void_fraction_one_sphere():
     atoms = [(2,2,2,1)]
-    assert calculate_void_fraction(atoms, (4,4,4)) == approx((4**3 - 4*math.pi/3) / 4**3, 0.01)
+    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - 4*math.pi/(3*4**3), 0.01)
+
+def test_void_fraction_two_identical_spheres():
+    atoms = [(2,2,2,1), (2,2,2,1)]
+    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - 4*math.pi/(3*4**3), 0.01)
+
+def test_void_fraction_two_separate_spheres():
+    atoms = [(1,1,1,1), (2.9,2.9,2.9,1)] #2.9 so it doesn't hit the right boundary
+    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - 2*4*math.pi/(3*4**3), 0.01)
