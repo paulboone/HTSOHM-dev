a774881688dcc94f8a668d6650cea307226c9d9d
Paul Boone <narcissus.pong@gmail.com>
2019-07-02 12:17:43 -0400

    possibly working PBCs

--- htsohm/void_fraction.py
+++ htsohm/void_fraction.py
@@ -1,6 +1,6 @@
 
 import itertools
-import math
+from math import sqrt, ceil
 
 import numpy as np
 
@@ -18,10 +18,15 @@ def calculate_void_fraction(atoms, box, points_per_angstrom=10):
     lattice_fill = np.zeros((xi_max, yi_max, zi_max))
 
     for x, y, z, r in atoms:
-        lattice_indices = itertools.product(range(xi_max), range(yi_max), range(zi_max))
+        delt_i = ceil(r / dx)
+        xi = int(x // dx); yi = int(y // dy); zi = int(z // dz)
+        lattice_indices = itertools.product(range(xi - delt_i, xi + delt_i + 1),
+                                            range(yi - delt_i, yi + delt_i + 1),
+                                            range(zi - delt_i, zi + delt_i + 1))
+
         for lxi, lyi, lzi in lattice_indices:
             lx = lxi * dx; ly = lyi * dy; lz = lzi * dz
-            if math.sqrt((lx - x)**2 + (ly - y)**2 + (lz -z)**2) < r:
-                lattice_fill[lxi, lyi, lzi] = 1.0
+            if sqrt((lx - x)**2 + (ly - y)**2 + (lz -z)**2) < r:
+                lattice_fill[lxi % xi_max, lyi % xi_max, lzi % xi_max] = 1.0
 
     return 1 - np.sum(lattice_fill) / (xi_max * yi_max * zi_max)
--- tests/test_void_fraction.py
+++ tests/test_void_fraction.py
@@ -5,6 +5,8 @@ from pytest import approx
 
 from htsohm.void_fraction import calculate_void_fraction
 
+r1volume = 4*math.pi/3
+
 def test_void_fraction_empty_crystal():
     assert calculate_void_fraction([], (4,4,4)) == 1.0
 
@@ -13,12 +15,16 @@ def test_void_fraction_huge_atom():
 
 def test_void_fraction_one_sphere():
     atoms = [(2,2,2,1)]
-    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - 4*math.pi/(3*4**3), 0.01)
+    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - r1volume/4**3, 0.01)
 
 def test_void_fraction_two_identical_spheres():
     atoms = [(2,2,2,1), (2,2,2,1)]
-    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - 4*math.pi/(3*4**3), 0.01)
+    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - r1volume/4**3, 0.01)
 
 def test_void_fraction_two_separate_spheres():
     atoms = [(1,1,1,1), (2.9,2.9,2.9,1)] #2.9 so it doesn't hit the right boundary
-    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - 2*4*math.pi/(3*4**3), 0.01)
+    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - 2*r1volume/4**3, 0.01)
+
+def test_void_fraction_one_sphere_across_pbc():
+    atoms = [(0,0,0,1)]
+    assert calculate_void_fraction(atoms, (4,4,4)) == approx(1 - r1volume/4**3, 0.01)
