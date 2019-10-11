438b9e619d86c5e0ab672993d5644079db83e312
Paul Boone <narcissus.pong@gmail.com>
2019-10-11 11:59:27 -0400

    add script to add bins to the CSV post run

--- htsohm/bin/output_csv.py
+++ htsohm/bin/output_csv.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import joinedload
 
 from htsohm import db
 from htsohm.db import Material
+from htsohm.htsohm_serial import calc_bin
 
 @click.command()
 @click.argument('database-path', type=click.Path())
@@ -28,6 +29,21 @@ def output_csv(database_path, start_id=0):
             m.void_fraction[0].void_fraction, m.void_fraction[0].void_fraction_geo,
             m.gas_loading[0].absolute_volumetric_loading, m.gas_loading[0].absolute_volumetric_loading_error
         ])
+
+@click.command()
+@click.argument('csv-path', type=click.Path())
+@click.option('-b', '--bin', nargs=4, type=click.Tuple([int, float, float, int]), multiple=True, help="column lower_bound upper_bound num_bins")
+def csv_add_bin(csv_path, bin):
+    with open(csv_path) as f:
+        csv_in = csv.reader(f)
+        csv_out = csv.writer(sys.stdout, lineterminator="\n")
+        header = next(csv_in)
+
+        bin_col_labels = ["bin%d" % col for col, _, _, _ in bin]
+        csv_out.writerow(header + bin_col_labels)
+        for row in csv_in:
+            calcd_bins = [calc_bin(float(row[col]), lb, ub, nb) for col, lb, ub, nb in bin]
+            csv_out.writerow(row + calcd_bins)
 
 if __name__ == '__main__':
     output_csv()
