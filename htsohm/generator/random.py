f8d921db384b2a62be4b1fbe2cc235d2888f34ff
Paul Boone <narcissus.pong@gmail.com>
2019-10-18 14:12:09 -0400

    further simplify random material generation

--- htsohm/generator/random.py
+++ htsohm/generator/random.py
@@ -10,28 +10,25 @@ def new_material(config):
         epsilon = uniform(*config["epsilon_limits"])
     ) for i in range(config["number_of_atom_types"])]
 
-    # randomly generate structure
-    structure=Structure(
-        a = uniform(*config["lattice_constant_limits"]),
-        b = uniform(*config["lattice_constant_limits"]),
-        c = uniform(*config["lattice_constant_limits"]),
-        lennard_jones = lj_atom_types
-    )
-
     # assign atom-site positions
     if "fix_atoms" in config:
         number_of_atoms = config['fix_atoms']
     else:
         number_of_atoms = random_number_density(config["number_density_limits"], structure)
-    
-    for i in range(number_of_atoms):
-        atom_type_index = choice(range(config["number_of_atom_types"]))
-        structure.atom_sites.append(
-                AtomSite(
-                    lennard_jones = lj_atom_types[atom_type_index],
-                    x = random(), y = random(), z = random(),
-                    q = 0.0))
 
-    m = Material(structure=structure, number_density=number_of_atoms / structure.volume)
+    atom_sites = [AtomSite(
+        lennard_jones = lj_atom_types[choice(range(config["number_of_atom_types"]))],
+        x = random(), y = random(), z = random(),
+        q = 0.0
+    ) for i in range(number_of_atoms)]
 
-    return m
+    # randomly generate structure
+    structure=Structure(
+        a = uniform(*config["lattice_constant_limits"]),
+        b = uniform(*config["lattice_constant_limits"]),
+        c = uniform(*config["lattice_constant_limits"]),
+        lennard_jones = lj_atom_types,
+        atom_sites = atom_sites
+    )
+
+    return Material(structure=structure, number_density=number_of_atoms / structure.volume)
