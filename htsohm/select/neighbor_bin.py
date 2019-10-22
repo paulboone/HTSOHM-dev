91c9f87e57c3d18675d0173d867834089d00c9ec
Paul Boone <narcissus.pong@gmail.com>
2019-10-22 14:39:26 -0400

    convert neighbor bin to be a binary on/off, if there are empty neighbors

--- htsohm/select/neighbor_bin.py
+++ htsohm/select/neighbor_bin.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 from numpy.random import choice
 
@@ -5,24 +7,24 @@ from htsohm.select.density_bin import choose_parent_bins_from_weighted_bin_list
 from htsohm.db import Material
 
 
-def choose_parents(num_parents, box_d, box_range, bin_materials):
-    raw_bins = [(i, len(mats)) for i, mats in np.ndenumerate(bin_materials) if len(mats) > 0]
-    bins = []
-    r = 2
+def choose_parents(num_parents, box_d, box_range, bin_materials, r=1):
+    raw_bins = [i for i, mats in np.ndenumerate(bin_materials) if len(mats) > 0]
+
     imax = len(bin_materials)
     jmax = len(bin_materials[0])
-    for (ci, cj), mat_count in raw_bins:
+    bin_weights = np.zeros(len(raw_bins))
+    for bin_index, (ci, cj) in enumerate(raw_bins):
         ilo = max(ci - r, 0)
         ihi = min(ci + r, imax - 1)
         jlo = max(cj - r, 0)
         jhi = min(cj + r, jmax - 1)
         neighbor_count = 0
-        for i in range(ilo, ihi + 1):
-            for j in range(jlo, jhi + 1):
-                if len(bin_materials[i][j]) == 0:
-                    neighbor_count += 1
-        bins.append([(ci, cj), 25 - neighbor_count])
+        for i,j in product(range(ilo, ihi + 1), range(jlo, jhi + 1)):
+            if len(bin_materials[i][j]) == 0:
+                bin_weights[bin_index] = 1
+                break
 
+    bins = list(zip(raw_bins, bin_weights))
     parent_bins = choose_parent_bins_from_weighted_bin_list(bins, num_parents)
     parent_indices = [choice(bin_materials[bin[0]][bin[1]], 1)[0] for bin in parent_bins]
 
