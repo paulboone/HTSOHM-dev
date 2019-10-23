a1ebb40796030c620b9eefbb1e3697681b785a67
Paul Boone <narcissus.pong@gmail.com>
2019-10-23 09:49:51 -0400

    removed commented out code

--- htsohm/simulation/run_all.py
+++ htsohm/simulation/run_all.py
@@ -12,11 +12,10 @@ def run_all_simulations(material, config):
     Depending on properties specified in config, adds simulated data for helium
     void fraction, gas loading, heat of adsorption, surface area, and
     corresponding bins to row in database corresponding to the input-material.
-
     """
     for simulation_number in config["simulations"]:
         print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()))
         simulation_config = config["simulations"][simulation_number]
         getattr(simulate, simulation_config["type"]).run(material, simulation_config, config)
-        #material.update_from_dict(results)
+
     print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()))
