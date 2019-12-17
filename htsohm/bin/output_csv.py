0d6706629cd3efc146e4db82d7a6f10134aa6468
Paul Boone <narcissus.pong@gmail.com>
2019-12-17 15:17:23 -0500

    updated output_csv to new schema

--- htsohm/bin/output_csv.py
+++ htsohm/bin/output_csv.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import joinedload
 
 from htsohm import db
 from htsohm.db import Material
-from htsohm.htsohm_serial import calc_bin
+from htsohm.htsohm_run import calc_bin
 
 @click.command()
 @click.argument('database-path', type=click.Path())
@@ -16,7 +16,7 @@ def output_csv(database_path, start_id=0):
     db.init_database(db.get_sqlite_dbcs(database_path))
     session = db.get_session()
     mats = session.query(Material) \
-        .options(joinedload("structure").joinedload("lennard_jones")) \
+        .options(joinedload("structure").joinedload("atom_types")) \
         .options(joinedload("gas_loading")) \
         .options(joinedload("void_fraction")) \
         .filter(Material.id >= start_id).all()
@@ -25,7 +25,7 @@ def output_csv(database_path, start_id=0):
     f.writerow(["id", "parent_id",  "a", "b", "c", "sigma", "epsilon", "void_fraction", "void_fraction_geo", "absolute_volumetric_loading", "absolute_volumetric_loading_error"])
     for m in mats:
         f.writerow([m.id, m.parent_id, m.structure.a, m.structure.b, m.structure.c,
-            m.structure.lennard_jones[0].sigma, m.structure.lennard_jones[0].epsilon,
+            m.structure.atom_types[0].sigma, m.structure.atom_types[0].epsilon,
             m.void_fraction[0].void_fraction, m.void_fraction[0].void_fraction_geo,
             m.gas_loading[0].absolute_volumetric_loading, m.gas_loading[0].absolute_volumetric_loading_error
         ])
