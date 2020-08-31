f8fcdad3ae01b5b564a93d6f5da229794f1eae09
Paul Boone <narcissus.pong@gmail.com>
2020-08-30 22:08:22 -0400

    random materials should satisfy minimum distance requirements; adding an atom to a material should as well

--- htsohm/generator/random.py
+++ htsohm/generator/random.py
@@ -2,6 +2,7 @@ from math import ceil
 from random import choice, random, randrange, uniform, randint
 
 from htsohm.db import Material, Structure, AtomTypes, AtomSite
+from htsohm.max_pair_distance import min_pair_distance
 
 def random_atom_types(num_atom_types, config):
     return [AtomTypes(
@@ -16,21 +17,33 @@ def random_atom_sites(num_sites, atom_types):
         q = 0.0
     ) for i in range(num_sites)]
 
-def new_material(config):
-    a = uniform(*config["lattice_constant_limits"])
-    if config["lattice_cubic"]:
-        b = a
-        c = a
-    else:
-        b = uniform(*config["lattice_constant_limits"])
-        c = uniform(*config["lattice_constant_limits"])
+def new_material(config, attempts=10):
+    for _ in range(attempts):
+        a = uniform(*config["lattice_constant_limits"])
+        if config["lattice_cubic"]:
+            b = a
+            c = a
+        else:
+            b = uniform(*config["lattice_constant_limits"])
+            c = uniform(*config["lattice_constant_limits"])
 
-    structure=Structure(
-        a = a, b = b, c = c,
-        atom_types = random_atom_types(config["number_of_atom_types"], config),
-    )
+        structure=Structure(
+            a = a, b = b, c = c,
+            atom_types = random_atom_types(config["number_of_atom_types"], config),
+        )
 
-    number_of_atoms = randint(*config["num_atoms_limits"])
-    structure.atom_sites = random_atom_sites(number_of_atoms, structure.atom_types)
+        number_of_atoms = randint(*config["num_atoms_limits"])
+        structure.atom_sites = random_atom_sites(number_of_atoms, structure.atom_types)
 
-    return Material(structure=structure, number_density=number_of_atoms / structure.volume)
+        if structure.min_pair_distance * structure.a > config['minimum_site_distance']:
+            return Material(structure=structure, number_density=number_of_atoms / structure.volume)
+
+    raise(Exception("Failed to create a material that satisfied min site distance requirements in allowed number of attempts"))
+
+
+def find_atom_site_with_minimum_distance(current_positions, distance, uc_a, num_trials=100):
+    for _ in range(num_trials):
+        trial_pos = (random(), random(), random())
+        if min_pair_distance(current_positions + [trial_pos]) * uc_a > distance:
+            return trial_pos
+    return None
