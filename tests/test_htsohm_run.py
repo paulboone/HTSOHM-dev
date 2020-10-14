ebbe432d377738eb3d56e3ab11b2c2db6f511374
Paul Boone <narcissus.pong@gmail.com>
2020-10-13 20:59:02 -0400

    add test for restarting

--- tests/test_htsohm_run.py
+++ tests/test_htsohm_run.py
@@ -1,6 +1,7 @@
 
 import importlib.resources
 import os
+import shutil
 
 import pytest
 
@@ -12,3 +13,12 @@ def test_htsohm_run__runs(tmpdir):
     os.chdir(tmpdir)
     with importlib.resources.path(tests, "material_config.yaml") as config_path:
         htsohm_run(config_path, max_generations=2)
+
+@pytest.mark.slow
+def test_htsohm_run__restarts_ok(tmpdir):
+    os.chdir(tmpdir)
+    with importlib.resources.path(tests, "test_pm.db") as db_path:
+        shutil.copyfile(db_path, os.path.join(tmpdir, "pm.db"))
+
+    with importlib.resources.path(tests, "material_config.yaml") as config_path:
+        htsohm_run(config_path, restart_generation=0, max_generations=2)
