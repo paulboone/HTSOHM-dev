47745106cf4d58de43ea95e500437642abd1986c
Paul Boone <narcissus.pong@gmail.com>
2020-11-17 20:09:29 -0500

    adjust figure for production: make size bigger so it will fit in 1-column figure, use 5 colors for ch4/uc and center them at 0,1,2,3,4, up the resolution

--- analysis/figure3_ml_vs_vf.py
+++ analysis/figure3_ml_vs_vf.py
@@ -19,12 +19,14 @@ rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 def figure3_ml_vs_vf(csv_path):
     num_bins = 40
 
-    fig = plt.figure(figsize=(4,4))
+    # figure has to be a little "oversized" so that mpl makes it big enough to fill a 1-column fig.
+    fig = plt.figure(figsize=(4.5,4.5))
 
-    cm = matplotlib.cm.get_cmap("viridis")
+    # we only want 5 colors for ch4/uc, where each color is centered at 0,1,2,3,4 +-0.5.
+    cm = matplotlib.cm.get_cmap("viridis",5)
+
     points = pd.read_csv(csv_path)
     points['ch4_uc'] = points.absolute_volumetric_loading * (num_ch4_a3 * points.a * points.b * points.c)
-
     ax = fig.subplots(ncols=1)
 
     ax.set_xlim(prop1range[0], prop1range[1])
@@ -38,19 +40,18 @@ def figure3_ml_vs_vf(csv_path):
     ax.tick_params(axis='y', which='major', labelsize=fs)
 
     ax.grid(which='major', axis='both', linestyle='-', color='0.9', zorder=0)
-    # ax.grid(which='minor', axis='both', linestyle='-', color='0.9', zorder=0)
 
     sc = ax.scatter(points.void_fraction_geo, points.absolute_volumetric_loading, zorder=2,
-                alpha=0.6, s=points.a, edgecolors=None, linewidths=0, c=points.ch4_uc.round(),
-                cmap=cm)
-
+                alpha=0.6, s=points.a, edgecolors=None, linewidths=0, c=points.ch4_uc,
+                cmap=cm, vmin=-0.5, vmax=4.5)
 
     ax.set_xlabel('Void Fraction', fontsize=fsl)
     ax.set_ylabel('Methane Loading [V/V]', fontsize=fsl)
 
     # fig.subplots_adjust(wspace=0.05, hspace=0.05)
     output_path = "figure3.png"
-    fig.savefig(output_path, dpi=600, bbox_inches='tight')
+
+    fig.savefig(output_path, dpi=1200, bbox_inches='tight')
     plt.close(fig)
 
 if __name__ == '__main__':
