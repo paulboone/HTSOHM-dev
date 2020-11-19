9aba6d9e20e3acff241e8b29a1816f477bfa32f4
Paul Boone <narcissus.pong@gmail.com>
2020-11-18 20:27:52 -0500

    use google docstring

--- htsohm/void_fraction.py
+++ htsohm/void_fraction.py
@@ -5,22 +5,25 @@ from math import sqrt, ceil
 import numpy as np
 
 def calculate_void_fraction(atoms, box, points_per_angstrom=10, probe_r=0.0):
-    """calculates a geometric void fraction, given the atom coordinates and diameter and the box
-    size.
+    """calculates a geometric void fraction, given the atom coordinates and diameter and the box size.
 
-    Discretizes the box dimensions into individual cubes where there are `points_per_angstrom`
-    points per angstrom of the box. For each atom in `atoms`, all cubes within a distance equal to
-    the probe radius `probe_r` and the atom radius (atom diameter / 2) are marked as filled. The
+    Discretizes the box dimensions into individual cubes where there are points_per_angstrom
+    points per angstrom of the box. For each atom in atoms, all cubes within a distance equal to
+    the probe radius probe_r and the atom radius (atom diameter / 2) are marked as filled. The
     void fraction is simply the ratio of filled cubes to total cubes.
 
-    atoms: an array of tuples (x, y, z, d) containing the atom coordinates x, y, z and the
-        atom diameter d.
-    box: a tuple containing the length of each side of the box. The box is assumed to start
-        at (0,0,0).
-    points_per_angstrom: the number of points per angstrom of the box, which determines the
-        discretization of the box. The larger the number, the longer the calculations will take to
-        perform, and the more accurate the result will be. Default: 10.
-    probe_r: the radius of the probe. Default: 0.0 angstroms.
+    Args:
+        atoms: an array of tuples (x, y, z, d) containing the atom coordinates x, y, z and the
+            atom diameter d.
+        box: a tuple containing the length of each side of the box. The box is assumed to start
+            at (0,0,0).
+        points_per_angstrom: the number of points per angstrom of the box, which determines the
+            discretization of the box. The larger the number, the longer the calculations will take to
+            perform, and the more accurate the result will be. Default: 10.
+        probe_r: the radius of the probe. Default: 0.0 angstroms.
+
+    Returns:
+        float: the calculated void fraction.
     """
 
     xi_max = ceil(box[0] * points_per_angstrom)
