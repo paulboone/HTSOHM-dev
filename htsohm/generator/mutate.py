4198b5b241a06b6ea0a25cc72ca8edbb01683287
Paul Boone <narcissus.pong@gmail.com>
2020-11-24 14:35:41 -0500

    add density var that can be used instead of num atoms; reordered mutations so that lattice is modified first and bounded by the density limits

--- htsohm/generator/mutate.py
+++ htsohm/generator/mutate.py
@@ -1,4 +1,5 @@
-from random import choice, random, uniform
+from math import ceil, floor
+from random import choice, choices, random, uniform
 
 from htsohm.slog import slog
 from htsohm.generator.random import random_atom_sites, random_atom_types
@@ -53,22 +54,56 @@ def mutate_material(parent, config):
     slog("Perturbing: %s [%s]" % (child.perturbation, perturb))
     ms = config["mutation_strength"]
 
+    if perturb & {"lattice"}:
+        ll = config["lattice_constant_limits"]
+        if config["lattice_cubic"]:
+            new_a = perturb_unweighted(cs.a, ms * (ll[1] - ll[0]), ll)
+            # new_a needs to be bounded s.t. the # atoms doesn't violate the density limits
+            new_a = max(new_a, (len(cs.atom_sites) / config["density_limits"][1])**(1/3))
+            new_a = min(new_a, (len(cs.atom_sites) / config["density_limits"][0])**(1/3))
+
+            cs.a = new_a
+            cs.b = new_a
+            cs.c = new_a
+        else:
+            raise(Exception("change needs to be made to support non-cubic lattices..."))
+            cs.b = perturb_unweighted(cs.b, ms * (ll[1] - ll[0]), ll)
+            cs.c = perturb_unweighted(cs.c, ms * (ll[1] - ll[0]), ll)
+
+    if perturb & {"num_atoms"}:
+        if random() < ms:
+            if random() < 0.5: # remove an atoms
+                if len(cs.atom_sites) > config['num_atoms_limits'][0]:
+                    site_to_remove = choice(cs.atom_sites)
+                    slog("Removing atom site: ", site_to_remove)
+                    cs.atom_sites.remove(site_to_remove)
+            else: # add an atom
+                if len(cs.atom_sites) < config['num_atoms_limits'][1]:
+                    slog("Adding atom site...")
+                    cs.atom_sites += random_atom_sites(1, cs.atom_types)
+    elif perturb & {"density"}:
+        dl = config["density_limits"]
+        number_density = perturb_unweighted(cs.number_density, ms * (dl[1] - dl[0]), dl)
+        number_of_atoms = round(number_density * cs.volume)
+
+        # num atoms needs to be bounded s.t. any integer # is within the density limit range, not just outside it
+        number_of_atoms = max(number_of_atoms, ceil(config["density_limits"][0] * cs.volume))
+        number_of_atoms = min(number_of_atoms, floor(config["density_limits"][1] * cs.volume))
+
+        delta_atoms = number_of_atoms - len(cs.atom_sites)
+        if delta_atoms < 0: # remove atoms
+            slog("Removing %d atom site(s)..." % delta_atoms)
+            for site_to_remove in choices(cs.atom_sites, k=delta_atoms):
+                cs.atom_sites.remove(site_to_remove)
+        elif delta_atoms > 0: # add atoms
+            slog("Adding %d atom site(s)..." % delta_atoms)
+            cs.atom_sites += random_atom_sites(delta_atoms, cs.atom_types)
+
+
     if config["number_of_atom_types"] > len(cs.atom_types):
         num_atom_types_to_add = config["number_of_atom_types"] - len(cs.atom_types)
         slog("Adding %d random atom types so we have number defined in the config" % num_atom_types_to_add)
         cs.atom_types += random_atom_types(num_atom_types_to_add, config)
-
-    if perturb & {"num_atoms"} and random() < ms:
-        if random() < 0.5: # remove an atoms
-            if len(cs.atom_sites) > config['num_atoms_limits'][0]:
-                site_to_remove = choice(cs.atom_sites)
-                slog("Removing atom site: ", site_to_remove)
-                removed_site = cs.atom_sites.remove(site_to_remove)
-        else: # add an atom
-            if len(cs.atom_sites) < config['num_atoms_limits'][1]:
-                slog("Adding atom site...")
-                cs.atom_sites += random_atom_sites(1, cs.atom_types)
-
 
     if perturb & {"atom_type_assignments"}:
         for i, atom in enumerate(cs.atom_sites):
@@ -84,17 +119,6 @@ def mutate_material(parent, config):
         for at in cs.atom_types:
             at.sigma = perturb_unweighted(at.sigma, ms * (sigl[1] - sigl[0]), sigl)
             at.epsilon = perturb_unweighted(at.epsilon, ms * (epsl[1] - epsl[0]), epsl)
-
-    if perturb & {"lattice"}:
-        ll = config["lattice_constant_limits"]
-        cs.a = perturb_unweighted(cs.a, ms * (ll[1] - ll[0]), ll)
-        if config["lattice_cubic"]:
-            cs.b = cs.a
-            cs.c = cs.a
-        else:
-            cs.b = perturb_unweighted(cs.b, ms * (ll[1] - ll[0]), ll)
-            cs.c = perturb_unweighted(cs.c, ms * (ll[1] - ll[0]), ll)
-        child.number_density = len(cs.atom_sites) / cs.volume
 
     if perturb & {"atom_sites"}:
         for a in cs.atom_sites:
--- htsohm/generator/random.py
+++ htsohm/generator/random.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import ceil, floor
 from random import choice, random, randrange, uniform, randint
 
 from htsohm.db import Material, Structure, AtomTypes, AtomSite
@@ -16,12 +16,15 @@ def random_atom_sites(num_sites, atom_types):
         q = 0.0
     ) for i in range(num_sites)]
 
+
+
 def new_material(config):
     a = uniform(*config["lattice_constant_limits"])
     if config["lattice_cubic"]:
         b = a
         c = a
     else:
+        raise(Exception("change needs to be made to support non-cubic lattices..."))
         b = uniform(*config["lattice_constant_limits"])
         c = uniform(*config["lattice_constant_limits"])
 
@@ -30,7 +33,16 @@ def new_material(config):
         atom_types = random_atom_types(config["number_of_atom_types"], config),
     )
 
-    number_of_atoms = randint(*config["num_atoms_limits"])
+    if "num_atoms_limits" in config:
+        number_of_atoms = randint(*config["num_atoms_limits"])
+    elif "density_limits" in config:
+        number_density = uniform(*config["density_limits"])
+        number_of_atoms = round(number_density * structure.volume)
+
+        # num atoms needs to be bounded s.t. any integer # is within the density limit range, not just outside it
+        number_of_atoms = max(number_of_atoms, ceil(config["density_limits"][0] * structure.volume))
+        number_of_atoms = min(number_of_atoms, floor(config["density_limits"][1] * structure.volume))
+
     structure.atom_sites = random_atom_sites(number_of_atoms, structure.atom_types)
 
     return Material(structure=structure, number_density=number_of_atoms / structure.volume)
